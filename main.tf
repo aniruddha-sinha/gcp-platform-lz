8b3a08b1388e38edbbfd85af5b7268ae62646bf3
Aniruddha Sinha <asinha0493@gmail.com>
2023-05-21 03:37:50 +0530

    removed the applied resources

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
-module "vpc" {
-  source = "./modules/vpc"
+# module "vpc" {
+#   source = "./modules/vpc"
 
-  project-id               = var.project_id
-  is-network-created       = false
-  regions                  = ["us-central1"]
-  ip-cidrs                 = ["10.0.130.0/24"]
-  source-tags              = []
-  target-tags              = []
-  enable-advanced-features = true
-}
+#   project-id               = var.project_id
+#   is-network-created       = false
+#   regions                  = ["us-central1"]
+#   ip-cidrs                 = ["10.0.130.0/24"]
+#   source-tags              = []
+#   target-tags              = []
+#   enable-advanced-features = true
+# }
 
-module "iam" {
-  source = "./modules/iam"
+# module "iam" {
+#   source = "./modules/iam"
 
-  project_id                   = var.project_id
-  service_account_id           = "gke-node-sa"
-  service_account_display_name = "GKE Node Service Account"
-  role_list = [
-    "roles/editor"
-  ]
-}
+#   project_id                   = var.project_id
+#   service_account_id           = "gke-node-sa"
+#   service_account_display_name = "GKE Node Service Account"
+#   role_list = [
+#     "roles/editor"
+#   ]
+# }
