61998d1bfff059bf7a78a5752cdc4145f3d048cc
Aniruddha Sinha <asinha0493@gmail.com>
2023-05-28 04:04:49 +0530

    changed local ip

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ module "atlas" {
 
   master_auth_config = {
     name    = "mac"
-    ip_addr = "45.237.0.12/32"
+    ip_addr = "49.37.54.176/32"
   }
 
   kubernetes_version = data.google_container_engine_versions.k8s_versions.release_channel_default_version["STABLE"]
