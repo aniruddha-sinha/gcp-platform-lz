0738232751ab86fabaf4b2fb59c1fd131f75a67b
Aniruddha Sinha <asinha0493@gmail.com>
2023-06-03 17:28:00 +0530

    added a node pool

--- main.tf
+++ main.tf
@@ -94,9 +94,7 @@ data "terraform_remote_state" "tf_cloud_remote_state" {
 # # Configure kubernetes provider with Oauth2 access token.
 # # https://registry.terraform.io/providers/hashicorp/google/latest/docs/data-sources/client_config
 # # This fetches a new token, which will expire in 1 hour.
-data "google_client_config" "default" {
-  project = var.project_id
-}
+data "google_client_config" "default" {}
 
 data "google_container_cluster" "atlas_cluster" {
   project  = var.project_id
