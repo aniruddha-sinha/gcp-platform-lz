1585c0323e3b540236362186a9e6d813ff077240
Aniruddha Sinha <asinha0493@gmail.com>
2023-06-03 17:36:22 +0530

    added a node pool

--- main.tf
+++ main.tf
@@ -103,7 +103,8 @@ data "google_container_cluster" "atlas_cluster" {
 }
 
 provider "kubernetes" {
-  host    = data.google_container_cluster.atlas_cluster.endpoint
+  host    = "https://${data.google_container_cluster.atlas_cluster.endpoint}"
+  
 
   token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(data.google_container_cluster.atlas_cluster.master_auth[0].cluster_ca_certificate)
