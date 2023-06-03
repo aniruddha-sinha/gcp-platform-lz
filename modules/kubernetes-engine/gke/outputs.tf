721b230fc120e3d1b9eda2cc6a904ae1de4cec7f
Aniruddha Sinha <asinha0493@gmail.com>
2023-06-03 18:01:53 +0530

    added a node pool

--- modules/kubernetes-engine/gke/outputs.tf
+++ modules/kubernetes-engine/gke/outputs.tf
@@ -2,6 +2,10 @@ output "cluster_name" {
   value = google_container_cluster.gke_cluster.name
 }
 
+output "endpoint" {
+  value = google_container_cluster.gke_cluster.endpoint
+}
+
 output "kubernetes_cluster_host" {
   value = google_container_cluster.gke_cluster.cluster_ipv4_cidr
 }
